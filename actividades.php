c2d114a43f170733927f8e075c4c2ade3f0fc9cf
François Miglianico <francois.miglianico@gmail.com>
2020-04-23 15:31:36 -0500

    Page activities

---
+++ actividades.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('sections/common/imports.html'); ?>
+    <title data-lang-key="activities.head.title"></title>
+</head>
+
+<body data-fade-in="true">
+
+    <?php 
+        include('sections/common/header.html');
+        include('sections/activities/hero.html');
+
+        include('sections/activities/activities.html');
+        include('sections/common/contact.html');
+
+        include('sections/common/footer.html');
+        include('sections/common/scripts.html'); 
+    ?>
+
+</body>
+
+</html>
